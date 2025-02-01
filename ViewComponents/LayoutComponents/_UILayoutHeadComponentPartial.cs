25d3ccffa61f03a37f1428720b743fc1d2ba641c
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-02-02 02:54:51 +0300

    Add project files.

---
+++ ViewComponents/LayoutComponents/_UILayoutHeadComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PufandMore.Web.ViewComponents.LayoutComponents
+{
+    public class _UILayoutHeadComponentPartial:ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/LayoutComponents/_UILayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PufandMore.Web.ViewComponents.LayoutComponents
+{
+    public class _UILayoutScriptComponentPartial:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
