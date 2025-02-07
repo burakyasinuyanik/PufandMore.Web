520da2431b55f43b022ad00a194b38203ee90e24
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-02-07 03:01:23 +0300

    footercomponent ok

---
+++ ViewComponents/LayoutComponents/_UILayoutFooterComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PufandMore.Web.ViewComponents.LayoutComponents
+{
+    public class _UILayoutFooterComponentPartial:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return View();
+        }
+    }
+}
